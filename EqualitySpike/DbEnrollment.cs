73d5c90df495f2407be7099fa698843f237dbe7b
bobvanh <bob.vanhoove@ru.nl>
2024-01-25 10:22:15 +0100

    Add project files.

---
+++ EqualitySpike/DbEnrollment.cs
@@ -0,0 +1,11 @@
+﻿namespace EqualitySpike;
+
+public class DbEnrollment : IEnrollmentId
+{
+    public string Code { get; set; } = null!;
+    public int Occasion { get; set; }
+    public string HabbieBabbie { get; set; } = null!;
+    public DateTime EnterDate { get; set; }
+
+    public override string ToString() => $"DbItem:{Code}-{Occasion}";
+}
---
+++ EqualitySpike/EnrollmentId.cs
@@ -0,0 +1,29 @@
+﻿namespace EqualitySpike;
+
+public interface IEnrollmentId
+{
+    string Code { get; }
+    int Occasion { get; }
+}
+
+public static class EnrollmentId
+{
+    public static Func<IEnrollmentId?, IEnrollmentId?, bool> AreEqual 
+        => (a, b) => a?.Code == b?.Code && a?.Occasion == b?.Occasion;
+
+    public static int GetHashCode(IEnrollmentId id) => $"{id.Code}{id.Occasion}".GetHashCode();
+
+    public static IEqualityComparer<IEnrollmentId> EqualityComparer 
+        => EqualityComparer<IEnrollmentId>.Create(AreEqual, GetHashCode);
+    public static int CompareById(IEnrollmentId a, IEnrollmentId b)
+    {
+        int codeOutcome = a.Code.CompareTo(b.Code);
+        int result = codeOutcome != 0 ? codeOutcome : a.Occasion.CompareTo(b.Occasion);
+        return result;
+    }
+
+    public static IComparer<IEnrollmentId> Comparer => Comparer<IEnrollmentId>.Create(CompareById);
+
+    public static Func<IEnrollmentId, bool> BuildEquals(IEnrollmentId b)
+        => a => AreEqual(a, b);
+}
