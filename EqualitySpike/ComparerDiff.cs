7cd82eb50fe3b3d506f9b23d7856d4ee77c55f98
Bob van Hoove <hoovebobvan@gmail.com>
2024-01-26 09:58:30 +0100

    Intended use case is for Incoming / Existing, will be in context where that's obvious. It's a diff that relies on IComparer hence the rename.
    Constructor is very boring, interesting code should be on top.

--- EqualitySpike/ComparerDiff.cs
+++ EqualitySpike/ComparerDiff.cs
@@ -2,39 +2,26 @@
 
 namespace EqualitySpike
 {
-    public class IncomingExistingDiff<TExisting, TIncoming, TKey>
+    public class ComparerDiff<TExisting, TIncoming, TKey>
         where TExisting : class, TKey
         where TIncoming : class, TKey
         where TKey : notnull
     {
-        private IncomingExistingDiff(
-            ImmutableSortedSet<TExisting> existingOnly, 
-            ImmutableSortedSet<TIncoming> incomingOnly, 
-            ImmutableSortedSet<TExisting> existingIntersection, 
-            ImmutableSortedSet<TIncoming> incomingIntersection)
-        {
-            ExistingOnly = existingOnly;
-            IncomingOnly = incomingOnly;
-            ExistingIntersection = existingIntersection;
-            IncomingIntersection = incomingIntersection;
-        }
-
         public ImmutableSortedSet<TExisting> ExistingOnly { get; }
         public ImmutableSortedSet<TIncoming> IncomingOnly { get; }
         public ImmutableSortedSet<TExisting> ExistingIntersection { get; }
         public ImmutableSortedSet<TIncoming> IncomingIntersection { get; }
 
-        public static IncomingExistingDiff<TExisting, TIncoming, TKey> Create(
-            IList<TExisting> existing, 
-            IList<TIncoming> incoming, 
+        public static ComparerDiff<TExisting, TIncoming, TKey> Create(
+            IList<TExisting> existing,
+            IList<TIncoming> incoming,
             IComparer<TKey> comparer)
         {
             var existingPerKey = existing.ToImmutableSortedDictionary(_ => _, _ => _, comparer);
             var incomingPerKey = incoming.ToImmutableSortedDictionary(_ => _, _ => _, comparer);
 
-            var sortedKeys = new SortedSet<TKey>(
-                existing.AsEnumerable<TKey>().Concat(incoming),
-                comparer);
+            var allKeys = existing.AsEnumerable<TKey>().Concat(incoming);
+            var sortedKeys = new SortedSet<TKey>(allKeys, comparer);
 
             var diff = sortedKeys.Aggregate(
                 new
@@ -65,11 +52,23 @@ namespace EqualitySpike
                     return acc;
                 });
 
-            return new IncomingExistingDiff<TExisting, TIncoming, TKey>(
-                diff.ExistingOnly.ToImmutableSortedSet<TExisting>(comparer), 
-                diff.IncomingOnly.ToImmutableSortedSet<TIncoming>(comparer), 
-                diff.ExistingIntersection.ToImmutableSortedSet<TExisting>(comparer), 
+            return new ComparerDiff<TExisting, TIncoming, TKey>(
+                diff.ExistingOnly.ToImmutableSortedSet<TExisting>(comparer),
+                diff.IncomingOnly.ToImmutableSortedSet<TIncoming>(comparer),
+                diff.ExistingIntersection.ToImmutableSortedSet<TExisting>(comparer),
                 diff.IncomingIntersection.ToImmutableSortedSet<TIncoming>(comparer));
+        }
+
+        private ComparerDiff(
+            ImmutableSortedSet<TExisting> existingOnly,
+            ImmutableSortedSet<TIncoming> incomingOnly,
+            ImmutableSortedSet<TExisting> existingIntersection,
+            ImmutableSortedSet<TIncoming> incomingIntersection)
+        {
+            ExistingOnly = existingOnly;
+            IncomingOnly = incomingOnly;
+            ExistingIntersection = existingIntersection;
+            IncomingIntersection = incomingIntersection;
         }
     }
 }
